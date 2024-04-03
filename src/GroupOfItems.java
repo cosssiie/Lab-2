5a4d9521121ac6da74c99bda13ef3d794833649c
Liza <ye.haponenko@ukma.edu.ua>
2024-04-03 14:21:36 +0300

    Add getters + toString to GroupOfItems class

--- src/GroupOfItems.java
+++ src/GroupOfItems.java
@@ -1,3 +1,21 @@
 public class GroupOfItems {
 
+     //*Назва групи товарів */
+     private String nameOfGroup;
+
+     //*Опис групи товарів */
+     private String descriptionOfGroup;
+
+     public GroupOfItems(String nameOfGroup, String descriptionOfGroup){
+        this.nameOfGroup = nameOfGroup;
+        this.descriptionOfGroup = descriptionOfGroup; 
+    }
+
+    public String toString(){
+        return "\nНазва групи товарів: " + nameOfGroup +
+        "\nОпис групи товарів: " + descriptionOfGroup;
+    }
+    
+
 }
+
