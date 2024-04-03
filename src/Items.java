a2d17734b695ac61f5a782e9f59b960dde238717
Liza <ye.haponenko@ukma.edu.ua>
2024-04-03 14:15:24 +0300

    Add getters + toString to Items class

--- src/Items.java
+++ src/Items.java
@@ -1,3 +1,56 @@
 public class Items {
 
+    //*Назва товару */
+    private String name;
+
+    //*Опис товару */
+    private String description;
+
+    //*Виробник товару */
+    private String producer;
+
+    //*Кількість товару на складі*/
+    private int count;
+
+    //*ціна за одиницю */
+    private double pricePerOne;
+
+    public Items(String name, String description, String producer, int count, double pricePerOne){
+        this.name = name;
+        this.description = description;
+        this.producer = producer;
+        this.count = count;
+        this.pricePerOne = pricePerOne;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public double getPricePerOne() {
+        return pricePerOne;
+    }
+
+    public String toString(){
+        return "\nНазва товару: " + name +
+        "\nОпис товару: " + description + 
+        "\nВиробник: " + producer + 
+        "\nКількість на складі: " + count + 
+        "\nЦіна за одиницю: " + pricePerOne;
+    }
+
+
 }
